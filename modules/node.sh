dcda82ff4a1bc20ea261d4e0cc2ac2ff9c50afcb
Vasyl Khrystyuk <h6.msangel@gmail.com>
2020-02-19 09:53:45 -0500

    Update node.sh

--- modules/node.sh
+++ modules/node.sh
@@ -5,3 +5,5 @@ npm install -g n
 if ! grep -q "NPM_PACKAGES" ~/.bashrc; then
     curl -LO https://raw.githubusercontent.com/glenpike/npm-g_nosudo/master/npm-g-nosudo.sh && printf "\ny\n" | sh npm-g-nosudo.sh
 fi
+# remove HARD
+# http://manpages.ubuntu.com/manpages/bionic/man1/removing-npm.1.html
