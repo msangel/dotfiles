7865292b451ad75e4e683fc9112dc0b8c6428f92
Vasyl Khrystyuk <h6.msangel@gmail.com>
2020-02-24 17:19:07 -0500

    Update node.sh

--- modules/node.sh
+++ modules/node.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-apt install npm -q -y
-npm install -g n
-/usr/local/bin/n latest
-if ! grep -q "NPM_PACKAGES" ~/.bashrc; then
-    curl -LO https://raw.githubusercontent.com/glenpike/npm-g_nosudo/master/npm-g-nosudo.sh && printf "\ny\n" | sh npm-g-nosudo.sh
-fi
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash
+nvm install 10.19.0
+nvm alias default 10.19.0
+
+# apt install npm -q -y
+# npm install -g n
+# /usr/local/bin/n latest
+# if ! grep -q "NPM_PACKAGES" ~/.bashrc; then
+#     curl -LO https://raw.githubusercontent.com/glenpike/npm-g_nosudo/master/npm-g-nosudo.sh && printf "\ny\n" | sh npm-g-nosudo.sh
+# fi
 # remove HARD
 # http://manpages.ubuntu.com/manpages/bionic/man1/removing-npm.1.html
