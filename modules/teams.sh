b434008f7aa988898f7c616addddf8d50656f5cf
Vasyl Khrystyuk <h6.msangel@gmail.com>
2020-05-14 16:14:17 -0400

    Update teams.sh

--- modules/teams.sh
+++ modules/teams.sh
@@ -1,1 +1,3 @@
 # https://teams.microsoft.com/downloads. 
+# https://packages.microsoft.com/repos/ms-teams/dists/stable/main/binary-amd64/
+# https://packages.microsoft.com/repos/ms-teams/pool/main/t/teams-insiders/
