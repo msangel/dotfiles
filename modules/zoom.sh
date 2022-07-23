6f9344705d25d58b7f6b35a75e4b60441360e8a0
Vasyl Khrystyuk <h6.msangel@gmail.com>
2022-07-23 20:20:58 +0300

    Update zoom.sh

--- modules/zoom.sh
+++ modules/zoom.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
- apt install libxcb-xtest0 ibus dconf-cli gir1.2-ibus-1.0 libibus-1.0-5 -y -q
+ apt install libxcb-xtest0 ibus dconf-cli gir1.2-ibus-1.0 libibus-1.0-5 libgl1-mesa-glx libegl1-mesa -y -q
 
 if ! dpkg -l | grep -q zoom; then
     wget https://zoom.us/client/latest/zoom_amd64.deb
