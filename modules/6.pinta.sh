a9763e6802f34ac777472de35f2a34bee2cbd93e
Vasyl Khrystyuk <h6.msangel@gmail.com>
2022-07-23 20:29:13 +0300

    Update and rename pinta.sh to 6.pinta.sh

--- modules/6.pinta.sh
+++ modules/6.pinta.sh
@@ -7,7 +7,7 @@ if ! dpkg -l | grep -q pinta; then
   sudo apt install gnupg ca-certificates  -q -y
   sudo add-apt-repository -y ppa:pinta-maintainers/pinta-stable
   sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-  echo "deb https://download.mono-project.com/repo/ubuntu stable-bionic main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
+  echo "deb https://download.mono-project.com/repo/ubuntu $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
 
   sudo apt-get update
 
