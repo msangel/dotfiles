7c11ca166817a58a0b0b988ce63a4c7055a56230
Vasyl Khrystyuk <h6.msangel@gmail.com>
2022-07-23 20:39:36 +0300

    Update 09.skype.sh

--- modules/09.skype.sh
+++ modules/09.skype.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # skype
 if ! dpkg -l | grep -q skypeforlinux; then
+    apt install libgdk-pixbuf-xlib-2.0-0 libgdk-pixbuf2.0-0 -y -q
     wget https://repo.skype.com/latest/skypeforlinux-64.deb
     dpkg -i skypeforlinux-64.deb
     rm skypeforlinux-64.deb
