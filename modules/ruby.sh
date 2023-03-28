3c4b9b98804a3d9501d42cb2256c7711113646d7
Vasyl Khrystyuk <h6.msangel@gmail.com>
2023-03-28 15:51:25 +0300

    Update ruby.sh

--- modules/ruby.sh
+++ modules/ruby.sh
@@ -7,6 +7,11 @@ gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D69
 curl -sSL https://get.rvm.io | bash -s stable
 sudo usermod -a -G rvm $USER
 
+# for ubuntu 22.04 the openssl3 is default. so for rubies must be compatible version, 
+# at this moment it`s installing openssl1.0 into user-defined folder (default to $HOME/.rvm/usr)
+rvm pkg install openssl
+rvm install ruby-3.0.0 --with-openssl-dir=$HOME/.rvm/usr
+
 # do this after restart/relogin
 # As of writing this ruby-2.6.4 is the most recent version, we will use this and set it to our default version.
 # rvm install 2.6.4
