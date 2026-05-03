4416b622051b7efd0b2adbb026b33a716fb3a540
Vasyl Khrystyuk <h6.msangel@gmail.com>
2026-05-03 05:24:15 +0300

    Delete modules/10.teams.sh

--- modules/10.teams.sh
+++
@@ -1,3 +0,0 @@
-# https://teams.microsoft.com/downloads. 
-# https://packages.microsoft.com/repos/ms-teams/dists/stable/main/binary-amd64/
-# https://packages.microsoft.com/repos/ms-teams/pool/main/t/teams-insiders/
